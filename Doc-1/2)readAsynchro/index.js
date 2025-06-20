cecbcd07f79d31173bf1c713fb8bba2c53f4442c
ThrishikK <kosuruthrishik@gmail.com>
2025-06-20 13:06:37 +0530

    Started Doc4 ExpressJs

--- Doc-1/2)readAsynchro/index.js
+++ Doc-1/2)readAsynchro/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("../readSynchro/WaterFront.tt", "utf-8", (error, data) => {
+fs.readFile("./WaterFront.txt", "utf-8", (error, data) => {
   if (error) return console.log(error);
   console.log(data);
 });
